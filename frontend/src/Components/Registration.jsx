dca0b2f1ec8ac3807a68a7c3d863058dfcb756e3
Neetigupta274 <guptaneeti042@gmail.com>
2024-12-25 11:58:03 +0530

    register

--- frontend/src/Components/Registration.jsx
+++ frontend/src/Components/Registration.jsx
@@ -136,18 +136,6 @@ export default function Registration() {
           />
         </div>
 
-        {/* Role */}
-        {/* <div className="mb-4">
-          <label className="block text-gray-900 font-medium mb-2">Role</label>
-          <input
-            type="text"
-            name="role"
-            value={formData.role}
-            onChange={handleChange}
-            placeholder="Enter your role"
-            className="w-full border rounded-md px-3 py-2 bg-white"
-          />
-        </div> */}
 
         {/* Address */}
         <div className="mb-4">
